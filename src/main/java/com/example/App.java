5910f838a73388922e863bfc9dd3ebaa1c47416f
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-17 10:25:46 +0200

    init project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("primary.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("JavaFX App");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
