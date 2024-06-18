e4b973b85c0044d0bed851fa736883f626a82032
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-18 10:19:04 +0200

    Add button To go to the different page and quit button

--- src/main/java/com/example/Menu.java
+++ src/main/java/com/example/Menu.java
@@ -2,6 +2,7 @@ package com.example;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -12,17 +13,63 @@ public class Menu extends Pane {
     }
 
     public void display() {
+        int buttonWidth = 120; 
+        int buttonHeight = 10;
+        int xAlign = 140;
+
+        Label label = new Label("Choose an option:");
+        label.setLayoutX(xAlign);
+        label.setLayoutY(50);
+        this.getChildren().add(label);
+
+        Button ASCIIButton = new Button("ASCII");
+        ASCIIButton.setPrefSize(buttonWidth, buttonHeight);
+        ASCIIButton.setLayoutX(xAlign);
+        ASCIIButton.setLayoutY(75);
+        this.getChildren().add(ASCIIButton);
 
         Button cesarButton = new Button("Cesar");
-        cesarButton.setLayoutX(175);
-        cesarButton.setLayoutY(100);
+        cesarButton.setPrefSize(buttonWidth, buttonHeight);
+        cesarButton.setLayoutX(xAlign);
+        cesarButton.setLayoutY(125);
         this.getChildren().add(cesarButton);
 
+        Button cesarAndASCIIButton = new Button("Cesar and ASCII");
+        cesarAndASCIIButton.setPrefSize(buttonWidth, buttonHeight);
+        cesarAndASCIIButton.setLayoutX(xAlign);
+        cesarAndASCIIButton.setLayoutY(175);
+        this.getChildren().add(cesarAndASCIIButton);
 
+        Button quitButton = new Button("Quit");
+        quitButton.setPrefSize(buttonWidth, buttonHeight);
+        quitButton.setLayoutX(xAlign);
+        quitButton.setLayoutY(225);
+        this.getChildren().add(quitButton);
 
+        ASCIIButton.setOnAction(e -> {
+            return; 
+            /* ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+             * A FAIRE ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+             */ ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        });
         cesarButton.setOnAction(e -> {
             Cesar cesar = new Cesar((Stage) this.getScene().getWindow());
             cesar.show((Stage) this.getScene().getWindow());
+        });
+
+        cesarAndASCIIButton.setOnAction(e -> {
+            return; 
+            /* ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+             * A FAIRE ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+             */ ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        });
+
+        quitButton.setOnAction(e -> {
+            System.exit(0);
         });
     }
 
