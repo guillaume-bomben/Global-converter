c62b13feb9b76fce8431d2c9b5cc54f3305538eb
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-19 10:43:25 +0200

    Add manage UpperCase Char for cesar code

--- src/main/java/com/example/Cesar.java
+++ src/main/java/com/example/Cesar.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 
 public class Cesar {
     private final ArrayList<Character> alphabet = new ArrayList<>(Arrays.asList('a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'));
-    
+    private final ArrayList<Character> ALPHABET = new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'));
+
     public String Encrypt_And_Decrypt(String type, String text, int key) {
         StringBuilder result = new StringBuilder();
         if (type.equals("Encrypt")) {
@@ -14,7 +15,13 @@ public class Cesar {
                     int index = alphabet.indexOf(character);
                     int newIndex = (index + key) % 26;
                     result.append(alphabet.get(newIndex));
-                } else {
+                } 
+                else if (ALPHABET.contains(character)) {
+                    int index = ALPHABET.indexOf(character);
+                    int newIndex = (index + key) % 26;
+                    result.append(ALPHABET.get(newIndex));
+                }
+                else {
                     result.append(character); // Caractères non alphabétiques
                 }
             }
@@ -27,7 +34,16 @@ public class Cesar {
                         newIndex = 26 + newIndex;
                     }
                     result.append(alphabet.get(newIndex));
-                } else {
+                }
+                else if (ALPHABET.contains(character)) {
+                    int index = ALPHABET.indexOf(character);
+                    int newIndex = (index - key) % 26;
+                    if (newIndex < 0) {
+                        newIndex = 26 + newIndex;
+                    }
+                    result.append(ALPHABET.get(newIndex));
+                }
+                else {
                     result.append(character); // Caractères non alphabétiques
                 }
             }
